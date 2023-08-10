b761fe6275cf384f0cee5e6337bd25a613e1656a
Alex O Bunnyshell <alexandru.oprisan@bunnyshell.com>
2023-08-11 00:44:36 +0200

    improve entrypoint script to run migrations

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -5,30 +5,61 @@ set -e
 workdir=$(pwd);
 
 # run warmup scripts (cache with new env variables)
-echo "caching config"
-cd $workdir && php artisan config:cache;
-
-if [ "${RUN_WARMUP:-false}" = "true" ]; then
-  echo "running neo-warmup scripts ..."
-  #cd $workdir && composer run-script warmup;
-else
-  echo "RUN_WARMUP set to false, skip warmup scripts"
-fi
-
-# wait for DB to be ready
-#timeout 3m sh -c 'until bin/console dbal:run-sql "SELECT 1" > /dev/null 2>&1; do echo "Waiting for database connection..." && sleep 5; done'
-#if [ $? -ne 0 ]; then
-#  echo "timed out while waiting for database to be ready"
-#  exit 1
-#fi
 
 # run pre-release scripts
-if [ "${RUN_PRE_RELEASE:-false}" = "true" ]; then
-  echo "running pre-release scripts ..."
-	#cd $workdir && composer run-script pre-release;
+if [ "${RUN_WARMUP:-false}" = "true" ]; then
+    echo "caching config"
+    cd $workdir && php artisan config:cache;
+
+    echo "caching routes"
+    cd $workdir && php artisan route:cache;
+
+    echo "caching views"
+    cd $workdir && php artisan view:cache;
+
+    echo "caching events"
+    cd $workdir && php artisan event:cache;
+
+    echo "caching packages"
+    cd $workdir && php artisan package:discover;
+
+    echo "caching bootstrap"
+    cd $workdir && php artisan optimize;
+
+    # run warmup scripts (cache with new env variables)
+    echo "caching config"
+    cd $workdir && php artisan config:cache;
+
 else
-  echo "RUN_PRE_RELEASE set to false, skip pre-release scripts"
+  echo "RUN_WARMUP set to false, skipping RUN_WARMUP scripts"
 fi
+
+
+
+# wait for DB to be ready
+timeout 3m sh -c 'php artisan db:monitor > /dev/null 2>&1; do echo "Waiting for database connection..." && sleep 5; done'
+if [ $? -ne 0 ]; then
+  echo "timed out while waiting for database to be ready"
+  exit 1
+fi
+
+# run migrations
+if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
+  echo "running migrations ..."
+	cd $workdir && php artisan db:migrate;
+else
+  echo "RUN_MIGRATIONS set to false, skipping migrations"
+fi
+
+
+# run seeding
+if [ "${RUN_SEEDING:-false}" = "true" ]; then
+  echo "seeding the database ..."
+	cd $workdir && php artisan db:seed;
+else
+  echo "RUN_SEEDING set to false, skipping seeding scripts"
+fi
+
 
 # execute the CMD
 exec /usr/local/bin/docker-php-entrypoint "$@"
