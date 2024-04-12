8ea394c2d0f15e04404065f466352903763f6d04
Alex O Bunnyshell <eaasmigrations@bunnyshell.com>
2024-04-12 18:45:52 +0300

    force HTTPS urls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // all routes will be forced to use https
+        if (true) {
+            URL::forceScheme('https');
+        }
     }
 }
